95deab80e7c65f716189b71430f44614a9d80a7c
Jonathan <djgrovezone@gmail.com>
2024-01-22 20:21:27 +0100

    Showing pipe response

--- src/app/Components/comments/comment/comment.component.ts
+++ src/app/Components/comments/comment/comment.component.ts
@@ -1,12 +1,13 @@
 import { Comment } from '../comments.component';
 import { ReplyButtonComponent } from './../../../Shared/Components/reply-button/reply-button.component';
 import { Component, Input } from '@angular/core';
+import { TimePipe } from "../../../Shared/Pipes/time.pipe";
 
 @Component({
-  selector: 'app-comment',
-  standalone: true,
-  imports: [ReplyButtonComponent],
-  templateUrl: './comment.component.html',
+    selector: 'app-comment',
+    standalone: true,
+    templateUrl: './comment.component.html',
+    imports: [ReplyButtonComponent, TimePipe]
 })
 export class CommentComponent {
   @Input() comment!: Comment;
