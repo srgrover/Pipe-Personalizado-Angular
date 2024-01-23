e509c64e1f57048ddce3d7c0933b432651e0ee31
Jonathan <djgrovezone@gmail.com>
2024-01-23 18:03:51 +0100

    Adding data transformation

--- src/app/Shared/Pipes/time.pipe.ts
+++ src/app/Shared/Pipes/time.pipe.ts
@@ -6,6 +6,40 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimePipe implements PipeTransform {
   transform(value: Date): string {
-    return 'Hola desde Pipe';
+    var today = new Date();
+    var diff = today.getTime() - value.getTime();
+
+    var secs = 1000;
+    var mins = secs * 60;
+    var hours = mins * 60;
+    var days = hours * 24;
+    var months = days * 30.416666666666668;
+    var years = months * 12;
+
+    var diffYears = Math.floor(diff / years);
+
+    diff = diff - diffYears * years;
+    var diffMonths = Math.floor(diff / months);
+
+    diff = diff - diffMonths * months;
+    var diffDays = Math.floor(diff / days);
+
+    diff = diff - diffDays * days;
+    var diffHours = Math.floor(diff / hours);
+
+    diff = diff - diffHours * hours;
+    var diffMins = Math.floor(diff / mins);
+
+    diff = diff - diffMins * mins;
+    var diffSecs = Math.floor(diff / secs);
+
+    return `Hace 
+    ${diffYears > 0 ? diffYears + ' años' : ''}
+    ${diffMonths > 0 ? diffMonths + ' meses' : ''}
+    ${diffDays > 0 ? diffDays + ' dias' : ''}
+    ${diffHours > 0 ? diffHours + ' horas' : ''}
+    ${diffMins > 0 ? diffMins + ' minutos' : ''}
+    ${diffSecs > 0 ? diffSecs + ' segundos' : ''}
+    `;
   }
 }
